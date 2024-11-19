60f19f09ff732d754c9f9a4b4b80e296da9f97d4
Elise Van Aerschot <elise.vanaerschot@student.ucll.be>
2024-11-19 13:59:33 +0100

    functie voeg citaat toe toegevoegd, css margin toegevoegd

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ document.querySelector('section.citaten').appendChild(artikel);
 
 /* element verwijderen */
 document.querySelector('#geencitaten').remove();
+
+/*nieuw citaat toevoegen*/
+const citaattitel2 = 'Vandaag';
+const citaattekst2 = "Wat je vandaag moet doen, moet je doen zoals je morgen denkt dat je het had moeten doen.";
+const citaatauteur2 = 'Toon Hermans';
+const citaattaal2 = 'nl';
+const artikel2 = document.createElement('article');
+artikel2.innerHTML = `<h2>${citaattitel2}</h2><p>${citaattekst2}</p><p class="auteur">${citaatauteur2}</p>`;
+artikel2.classList.add(`${citaattaal2}`);
+document.querySelector('section.citaten').appendChild(artikel2);
+
+
+
+function voegCitaatToe(titel, tekst, auteur, taal){
+    const citaatArtikel = document.createElement('article');
+    citaatArtikel.innerHTML = `<h2>${titel}</h2><p>${tekst}</p><p class="auteur">${auteur}</p>`;
+    citaatArtikel.classList.add(`${taal}`);
+    document.querySelector('section.citaten').appendChild(citaatArtikel);
+
+}
+voegCitaatToe('Penser', 'Je pense, donc je suis.', 'René Descartes', 'fr');
