26651509726f29a4c5e601e0515245290fecd170
Elise Van Aerschot <elise.vanaerschot@student.ucll.be>
2024-11-19 11:22:36 +0100

    h1 aangepast, eerste citaat toegevoegd

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Het werkt!!')
+/*testcode om uit te teste of script werkt : console.log('Het werkt!!')*/
+
+/* h1 aanpassen*/
+document.querySelector('h1').innerText = 'Inspirerende citaten';
+
+/* eerste citaat toevoegen */
+const citaattitel1 = 'To be';
+const citaattekst1 = "To be or not te be, that's the question";
+const citaatauteur1 = 'William Shakespeare';
+const citaattaal1 = 'en';
+const artikel = document.createElement('article');
+artikel.innerHTML = `<h2>${citaattitel1}</h2><p>${citaattekst1}</p><p class="auteur">${citaatauteur1}</p>`;
+artikel.classList.add(`${citaattaal1}`);
+document.querySelector('section.citaten').appendChild(artikel);
