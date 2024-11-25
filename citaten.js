f57c4f39ce418cbb330c272e516e9637a5f04de4
Elise Van Aerschot <elise.vanaerschot@student.ucll.be>
2024-11-25 12:32:51 +0100

    Object en array, vereenvoudigd

---
+++ citaten.js
@@ -0,0 +1,26 @@
+/* citaat array */
+const alleCitaten = [
+{
+    titel: 'Uitgelegd',
+    tekst: 'Als ik zou willen dat je het begreep, zou ik het beter hebben uitgelegd.',
+    auteur: 'Johan Cruijff',
+    taal: 'nl',
+},
+{
+    titel: 'Penser',
+    tekst: 'Je pense, donc je suis.',
+    auteur: 'René Descartes',
+    taal: 'fr',
+},
+{
+    titel: 'Vandaag',
+    tekst: "Wat je vandaag moet doen, moet je doen zoals je morgen denkt dat je het had moeten doen.",
+    auteur: 'Toon Hermans',
+    taal: 'nl',
+},
+{
+    titel: 'To be',
+    tekst: "To be or not te be, that's the question",
+    auteur: 'William Shakespeare',
+    taal: 'en',
+}];
--- script.js
+++ script.js
@@ -3,39 +3,34 @@
 /* h1 aanpassen*/
 document.querySelector('h1').innerText = 'Inspirerende citaten';
 
-/* eerste citaat toevoegen */
-const citaattitel1 = 'To be';
+/*/* eerste citaat toevoegen */
+/*const citaattitel1 = 'To be';
 const citaattekst1 = "To be or not te be, that's the question";
 const citaatauteur1 = 'William Shakespeare';
 const citaattaal1 = 'en';
 const artikel = document.createElement('article');
 artikel.innerHTML = `<h2>${citaattitel1}</h2><p>${citaattekst1}</p><p class="auteur">${citaatauteur1}</p>`;
 artikel.classList.add(`${citaattaal1}`);
-document.querySelector('section.citaten').appendChild(artikel);
+document.querySelector('section.citaten').appendChild(artikel);*/
 
 /* element verwijderen */
 document.querySelector('#geencitaten').remove();
 
-/*nieuw citaat toevoegen*/
-const citaattitel2 = 'Vandaag';
-const citaattekst2 = "Wat je vandaag moet doen, moet je doen zoals je morgen denkt dat je het had moeten doen.";
-const citaatauteur2 = 'Toon Hermans';
-const citaattaal2 = 'nl';
-const artikel2 = document.createElement('article');
-artikel2.innerHTML = `<h2>${citaattitel2}</h2><p>${citaattekst2}</p><p class="auteur">${citaatauteur2}</p>`;
-artikel2.classList.add(`${citaattaal2}`);
-document.querySelector('section.citaten').appendChild(artikel2);
 
 
-
-function voegCitaatToe(titel, tekst, auteur, taal){
+function voegCitaatObjectToe(cit){
     const citaatArtikel = document.createElement('article');
-    citaatArtikel.innerHTML = `<h2>${titel}</h2><p>${tekst}</p><p class="auteur">${auteur}</p>`;
-    citaatArtikel.classList.add(`${taal}`);
+    citaatArtikel.innerHTML = `<h2>${cit.titel}</h2><p>${cit.tekst}</p><p class="auteur">${cit.auteur}</p>`;
+    citaatArtikel.classList.add(`${cit.taal}`);
     document.querySelector('section.citaten').appendChild(citaatArtikel);
 
 }
-voegCitaatToe('Penser', 'Je pense, donc je suis.', 'René Descartes', 'fr');
+/*for (const citaat of alleCitaten ){
+    voegCitaatObjectToe(citaat);
+};*/
+
+alleCitaten.forEach(voegCitaatObjectToe);
+
 
 /*const footer = document.createElement('footer');
 footer.innerHTML = '<p>&copy; 2024 - Elise</p>';
