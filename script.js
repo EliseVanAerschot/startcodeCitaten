8bf03787753a462844800b3cbf5c96bd2a8fad02
Elise Van Aerschot <elise.vanaerschot@student.ucll.be>
2024-12-12 15:16:03 +0100

    elementen verwijderen, finIndex() arraymethode, png vervangen door svg + kleur toegevoegd

--- script.js
+++ script.js
@@ -27,9 +27,20 @@ function voegCitaatObjectToe(cit){
     const citaatArtikel = document.createElement('article');
     citaatArtikel.innerHTML = `<h2>${cit.titel}</h2><p>${cit.tekst}</p><p class="auteur">${cit.auteur}</p>`;
     citaatArtikel.classList.add(`${cit.taal}`);
+    const deleteKnop = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    deleteKnop.setAttribute('viewBox', '0 0 24 24');
+    deleteKnop.setAttribute('fill', 'none');
+    deleteKnop.setAttribute('width', '30px');
+    deleteKnop.setAttribute('heigth', '30px');
+    deleteKnop.innerHTML = '<path d="M10 11V17" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M14 11V17" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M4 7H20" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M6 7H12H18V18C18 19.6569 16.6569 21 15 21H9C7.34315 21 6 19.6569 6 18V7Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>';
+    citaatArtikel.insertAdjacentElement('afterbegin', deleteKnop);
     document.querySelector('section.citaten').appendChild(citaatArtikel);
-
-}
+    deleteKnop.addEventListener('click', 
+        () => {
+            //console.log('geklikt op ' + cit.titel )
+            verwijderCitaat(cit.titel);
+        });
+};
 /*for (const citaat of alleCitaten ){
     voegCitaatObjectToe(citaat);
 };*/
@@ -37,7 +48,7 @@ function voegCitaatObjectToe(cit){
 
 function toonAantalCitaten(){
     document.querySelector('#aantalCitaten').innerText = `Totaal aantal citaten: ${alleCitaten.length}`;
-}
+};
 
 
 
@@ -68,6 +79,15 @@ const nieuwCitaat = {
 /*Voeg nieuw citaat achteraan toe */
 alleCitaten.push(nieuwCitaat);
 
+function verwijderCitaat(verwijderTitel){
+    const plaats = alleCitaten.findIndex(cit => cit.titel === verwijderTitel
+    );
+    alleCitaten.splice(plaats,1);
+    citatenSectie.dispatchEvent(new CustomEvent('citatenUpdate'));
+    toonAantalCitaten();
+    toonLangsteCitaat();
+};
+
 const checkboxes = document.querySelectorAll('input[name=taal]');
   checkboxes.forEach((checkbox) => {checkbox.addEventListener('change', () => {
     citatenSectie.dispatchEvent(new CustomEvent('citatenUpdate'));
@@ -88,7 +108,6 @@ citatenSectie.addEventListener('citatenUpdate', toonCitaten);
 
 function toonCitaten() {
     const geslecteerdeTalen = maakTalenLijst();
-    console.log(geslecteerdeTalen);
     document.querySelector('section.citaten').innerHTML = '';
     alleCitaten.forEach((cit) => {
         if (cit.tekst.includes(document.querySelector('form.zoektekst > input[type=text]').value) && geslecteerdeTalen.includes(cit.taal)){
